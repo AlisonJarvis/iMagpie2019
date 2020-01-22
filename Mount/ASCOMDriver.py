88c65de5d7c85e212c4628682a7442665eb9a7fa
nathanhh <47604872+nathanhh@users.noreply.github.com>
2020-01-22 11:56:14 -0700

    Mount driver

--- Mount/ASCOMDriver.py
+++ Mount/ASCOMDriver.py
@@ -1,1 +1,17 @@
+def driver(ra, dec):
+    import win32com.client
+    mount = win32com.client.Dispatch("mountid")
+
+    if mount.Connected:
+        print "Mount already connected"
+    else:
+        mount.Connected = True
+        if mount.Connected:
+            print "Connected to mount"
+        else:
+            print "Unable to connect"
+
+    mount.Tracking = True
+    mount.SlewToCoordinates(ra, dec)
+    mount.Connected = False
 
