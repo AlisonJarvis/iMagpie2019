688b50f80796d278bc5be356490208546f82f2ae
Nathan Herr <Nathan@cu-biot-14-10.203.182.2.int.colorado.edu>
2020-02-05 13:30:25 -0700

    soltrack dickaround

---
+++ Soltrack Python Dickaround/yeehaw.py
@@ -0,0 +1,6 @@
+import soltrack
+
+# lol its just one line of code to track the sun
+soltrack.computeSunPosition()
+#  it'd be so sick if we could write this in python so we could use the existing function for mount command
+#  otherwise I have to write one in c :/
