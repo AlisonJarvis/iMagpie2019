6823404c4452a35fbba744a975d351ebb6b99c94
Nathan Herr <Nathan@cu-biot-14-10.203.182.227.int.colorado.edu>
2020-02-12 14:36:01 -0700

    driver updates

--- Mount/ASCOMDriver.py
+++ Mount/ASCOMDriver.py
@@ -1,24 +1,42 @@
-import win32com.client
-import datetime
+import RPi.GPIO as GPIO
+import serial
+
+# define button number
+buttonNum = 3  # customize for how we wire the Pi
 
 
-def driver(ra, dec):
-    mount = win32com.client.Dispatch("mountid")
+def setup():
+    # GPIO.setmode(GPIO.BOARD)  # sets board as mode
+    # GPIO.setup(buttonNum, GPIO.IN, GPIO.PUD_UP)  # defines button, input, and pull up (not sure what that last one
+    # means)
 
-    if mount.Connected:
-        print("Mount already connected")
-    else:
-        mount.Connected = True
-        if mount.Connected:
-            print("Connected to mount")
-        else:
-            print("Unable to connect")
+    # define serial port being used  (dependant on Pi number)
+    ser_port = "/dev/ttyUSB0"  # defining serial port as using USB (Linux standard)
 
-    # deal with time
-    # t = datetime.datetime.now()  #time now
+    # define settings
+    ser = serial.Serial(ser_port, 9600, timeout=5)
+    altaz = ser.write(":GAC#")  # check current alt-az
+    altlimit = ser.write(":GAL#")  # check altitude limit
 
-    # if tcommand >= t:
-    mount.SlewToCoordinates(ra, dec)
-    mount.Tracking = True
-    mount.Connected = False
+    ser.write(':RT0#')  # set sidereal tracking
+    ser.write("SG-360")  # set timezone
+    ser.write(":SDS0#")  # not DST
+
+    return [altaz, altlimit]
+
+
+def driver(alt, az):
+    #  Alt/az should be input as 8 character .01 arcsecond (idk exactly what this means)
+    #  Also could convert this within function
+    # define serial port being used  (dependant on Pi number)
+    ser_port = "/dev/ttyUSB0"  # defining serial port as using USB (Linux standard)
+
+    # define settings
+    ser = serial.Serial(ser_port, 9600, timeout=5)
+
+    ser.write(":SasTTTTTTTT#")  # define command altitude
+    ser.write(":SzTTTTTTTTT#")  # define command azimuth
+
+
+
 
