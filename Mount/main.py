fec9488b87647deabb1fea0acfdeca1c0498e1f6
Nate <nahe9064@colorado.edu>
2020-02-19 15:26:59 -0700

    trying stuff

--- Mount/main.py
+++ Mount/main.py
@@ -1,15 +1,15 @@
-import ASCOMDriver as ASCOM
 import time
 from Mount import Mount
 
-mount = Mount("/dev/tty.usbserial-AQ00LYCP",9600, 5)
+mount = Mount("/dev/ttyUSB0",9600, 5)
 
-#mount.stop_slew()
 #mount.calibrate()
 mount.slew_to_mech_zero()
-#mount.set_alt_az(30, 0)
-#mount.slew_to_coordinates()
-#time.sleep(10)
+time.sleep(15)
+mount.set_alt_az(90, 45)
+mount.slew_to_coordinates()
+time.sleep(20)
+mount.stop_slew()
 print(mount.current_alt_az())
 
 
