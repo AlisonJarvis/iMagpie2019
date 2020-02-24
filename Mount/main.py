7260964607a4aee2e697bbcaec80a573203970ff
Nathan Herr <Nathan@cu-biot-14-10.203.181.53.int.colorado.edu>
2020-02-24 13:48:48 -0700

    Nathan Herr is a big dumb dumb with idiot brains

--- Mount/main.py
+++ Mount/main.py
@@ -5,13 +5,12 @@ mount = Mount("/dev/tty.usbserial-AQ00LYCP",9600, 5)
 
 #mount.calibrate()
 mount.slew_to_mech_zero()
-time.sleep(15)
-#mount.set_alt_az(90, 45)
-#mount.set_ra_dec()
-#mount.slew_to_coordinates()
-#time.sleep(20)
-mount.stop_slew()
-print(mount.current_alt_az())
+# time.sleep(15)
+# mount.set_alt_az(45, 45)
+# mount.slew_to_coordinates()
+# time.sleep(20)
+# mount.stop_slew()
+# print(mount.current_alt_az())
 
 
 
