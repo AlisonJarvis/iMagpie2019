3b2fd76cee07904b24eb493f75e0c65506705068
Nate <nahe9064@colorado.edu>
2020-02-27 18:58:01 -0700

    functional

--- Astrometry-Raspi/astrometry.py
+++ Astrometry-Raspi/astrometry.py
@@ -1,21 +1,25 @@
 #!C:\iMagpie\venv\Scripts\python.exe
-# #import os #for using terminal
+import os #for using terminal
 import subprocess #for getting data out of terminal output
 
-RA = 13.43306 #Right Ascension in degrees
+RA = 201.5 #Right Ascension in degrees
 DEC = -10.82389 #Declination in degrees
 radius = 5 #radius in degrees of what to search around center RA and DEC
 Low = 5 #lower bound of arc second per pixel of picture
 Hi = 5.5 #Upper bound of arc second per pixel of picture
-sample = 2 #downsample the picture by this factor first before solving
+sample = 4 #downsample the picture by this factor first before solving
 
 
-cmd = "/usr/local/astrometry/bin sudo ./solve-field --ra %f --dec %f --radius %f -L %f -H %f -u arcsecperpix --downsample %d --no-plots -D ~/Desktop/_APP3220.solvage ~/Desktop/_APP3220.solvage/_APP3220.jpg" % (RA,DEC,radius,Low,Hi,sample,)
+os.chdir("/../../../../../usr/local/astrometry/bin")
+cmd = "./solve-field --ra %f --dec %f --radius %f -L %f -H %f -u arcsecperpix --downsample %f --no-plots --overwrite -D /home/pi/Desktop/Astrometry/APP3220solvage /home/pi/Desktop/Astrometry/APP3220solvage/APP3220.jpg" % (RA,DEC,radius,Low,Hi,sample)
 proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE) #saving terminal output to variables within python
-# os.system("cmd") #execute astrometry terminal command
+os.system(cmd) #execute astrometry terminal command
+
 output = proc.communicate()[0] #communicate with the opened process
-output = output.split(b"\n") #parsing into each line
-#output = output[0].split("\\") #dunno i was told this one
+output = output.decode()
+output = output.split("\n") #parsing into each line
+#utput = output[0].split("\\") #dunno i was told this one
+
 """
 for line in output: #parse line by line
     RA_Dec_index = output.find("Field center: (RA,Dec) =") #find when it solves to give field center
@@ -25,12 +29,18 @@ print("Field Rotation angle index = " + str(Field_rotation_index)) #find index w
 #print("RA and Dec Value = " +str
 """""
 
-for line in output:
-    if line.find(b"Field center: (RA,Dec) ="):
-        RA_Dec_Line = line
-    if line.find(b"Field rotation angle:"):
-        Rot_Angle_Line = line
-        break
 
-print(RA_Dec_Line)
-print(Rot_Angle_Line)
+for i in output:
+    if i.find("Field center: (RA,Dec) =")!=-1:
+       RA_Dec_Line = i
+    if i.find("Field rotation angle:")!=-1:
+        Rot_Angle_Line = i
+    
+RA_COF = float(RA_Dec_Line[26:36])
+Dec_COF = float(RA_Dec_Line[38:48])
+Rot_Angle = float(Rot_Angle_Line[28:36])
+
+print(RA_COF)
+print(Dec_COF)
+print(Rot_Angle)
+
