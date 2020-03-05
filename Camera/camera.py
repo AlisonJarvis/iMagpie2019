aceefd3c6ae3f2f9d2d45f910fb68264829e98a6
Chase Puskar <chasepuskar@cu-biot-15-10.203.184.165.int.colorado.edu>
2020-03-05 13:55:33 -0700

    v2 of working camera/video code

--- Camera/camera.py
+++ Camera/camera.py
@@ -14,6 +14,8 @@ __version__ = '0.1.0'
 __license__ = 'MIT'
 
 SIZE = (640, 480)
+WIDTH = 4656
+HEIGHT = 3520
 
 # ASI_IMGTYPE
 ASI_IMG_RAW8 = 0
@@ -30,74 +32,10 @@ def save_control_values(filename, settings):
             f.write('%s: %s\n' % (k, str(settings[k])))
     print('Camera settings saved to %s' % filename)
 
-def save_image(camera):
-    print('Enabling stills mode')
-    try:
-        # Force any single exposure to be halted
-        camera.stop_video_capture()
-        camera.stop_exposure()
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        pass
-    print('Capturing a single 8-bit mono image')
-    filename = 'image_mono.jpg'
-    camera.set_image_type(asi.ASI_IMG_RAW8)
-    camera.capture(filename=filename)
-    print('Saved to %s' % filename)
-    save_control_values(filename, camera.get_control_values())
-
-def start_video_feed(camera):
-    # Enable video mode
-    try:
-        # Force any single exposure to be halted
-        camera.stop_exposure()
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        pass
-
-    print('Enabling video mode')
-    camera.start_video_capture()
-
-    # Set the timeout, units are ms
-    timeout = (camera.get_control_value(asi.ASI_EXPOSURE)[0] / 1000) * 2 + 500
-    camera.default_timeout = timeout
-
-    # Set up window and display images
-    app = wx.App()
-    frame = Frame(camera)
-    frame.Center()
-    frame.Show()
-    app.MainLoop()
-
-def get_image(camera):
+def get_bitmap(camera):
     data = camera.get_video_data()
-    whbi = camera.get_roi_format()
-    shape = [whbi[1], whbi[0]]
-    if whbi[3] == ASI_IMG_RAW8 or whbi[3] == ASI_IMG_Y8:
-        img = np.frombuffer(data, dtype=np.uint8)
-    elif whbi[3] == ASI_IMG_RAW16:
-        img = np.frombuffer(data, dtype=np.uint16)
-    elif whbi[3] == ASI_IMG_RGB24:
-        img = np.frombuffer(data, dtype=np.uint8)
-        shape.append(3)
-    else:
-        raise ValueError('Unsupported image type')
-    img = img.reshape(shape)
-
-    mode = None
-    if len(img.shape) == 3:
-        img = img[:, :, ::-1]  # Convert BGR to RGB
-    if whbi[3] == ASI_IMG_RAW16:
-        mode = 'I;16'
-    image = Image.fromarray(img, mode=mode)
-    return image
-
-def pil_to_wx(image):
-    width, height = image.size
-    buffer = image.convert('RGB').tobytes()
-    bitmap = wx.Bitmap.FromBuffer(width, height, buffer)
+    buffer = np.repeat(bytes(data),3)
+    bitmap = wx.Bitmap.FromBuffer(WIDTH, HEIGHT, buffer)
     return bitmap
 
 class Panel(wx.Panel):
@@ -113,8 +51,7 @@ class Panel(wx.Panel):
         self.Update()
         wx.CallLater(15, self.update)
     def create_bitmap(self):
-        image = get_image(self.camera)
-        bitmap = pil_to_wx(image)
+        bitmap = get_bitmap(self.camera)
         return bitmap
     def on_paint(self, event):
         bitmap = self.create_bitmap()
@@ -129,76 +66,109 @@ class Frame(wx.Frame):
         panel = Panel(self)
         self.Fit()
 
-env_filename = os.getenv('ZWO_ASI_LIB')
 
-parser = argparse.ArgumentParser(description='Process and save images from a camera')
-parser.add_argument('filename',
-                    nargs='?',
-                    help='SDK library filename')
-args = parser.parse_args()
-#args.filename = '../ZWO_ASI_Library/lib/armv7/libASICamera2.so';
-args.filename = '../ZWO_ASI_Library/lib/mac/libASICamera2.dylib';
+class ImageSensor():
+    def __init__(self):
+        env_filename = os.getenv('ZWO_ASI_LIB')
+        parser = argparse.ArgumentParser(description='Process and save images from a camera')
+        parser.add_argument('filename',
+                            nargs='?',
+                            help='SDK library filename')
+        args = parser.parse_args()
+        #args.filename = '../ZWO_ASI_Library/lib/armv7/libASICamera2.so';
+        args.filename = '../ZWO_ASI_Library/lib/mac/libASICamera2.dylib';
+        # Initialize zwoasi with the name of the SDK library
+        if args.filename:
+            asi.init(args.filename)
+        elif env_filename:
+            asi.init(env_filename)
+        else:
+            print('The filename of the SDK library is required (or set ZWO_ASI_LIB environment variable with the filename)')
+            sys.exit(1)
+        num_cameras = asi.get_num_cameras()
+        if num_cameras == 0:
+            print('No cameras found')
+            sys.exit(0)
+        cameras_found = asi.list_cameras()  # Models names of the connected cameras
+        if num_cameras == 1:
+            camera_id = 0
+            print('Found one camera: %s' % cameras_found[0])
+        else:
+            print('Found %d cameras' % num_cameras)
+            for n in range(num_cameras):
+                print('    %d: %s' % (n, cameras_found[n]))
+            # TO DO: allow user to select a camera
+            camera_id = 0
+            print('Using #%d: %s' % (camera_id, cameras_found[camera_id]))
 
-# Initialize zwoasi with the name of the SDK library
-if args.filename:
-    asi.init(args.filename)
-elif env_filename:
-    asi.init(env_filename)
-else:
-    print('The filename of the SDK library is required (or set ZWO_ASI_LIB environment variable with the filename)')
-    sys.exit(1)
+        camera = asi.Camera(camera_id)
+        camera_info = camera.get_camera_property()
+        # Get all of the camera controls
+        print('')
+        print('Camera controls:')
+        controls = camera.get_controls()
+        for cn in sorted(controls.keys()):
+            print('    %s:' % cn)
+            for k in sorted(controls[cn].keys()):
+                print('        %s: %s' % (k, repr(controls[cn][k])))
+        # Use maximum USB bandwidth permitted
+        camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandWidth']['MaxValue'])
+        # Set some sensible defaults. They will need adjusting depending upon
+        # the sensitivity, lens and lighting conditions used.
+        camera.disable_dark_subtract()
+        camera.set_control_value(asi.ASI_GAIN, 0)
+        camera.set_control_value(asi.ASI_EXPOSURE, 10000)
+        camera.set_control_value(asi.ASI_WB_B, 99)
+        camera.set_control_value(asi.ASI_WB_R, 75)
+        camera.set_control_value(asi.ASI_GAMMA, 50)
+        camera.set_control_value(asi.ASI_BRIGHTNESS, 50)
+        camera.set_control_value(asi.ASI_FLIP, 0)
+        self.camera = camera
+    def save_image(self):
+        print('Enabling stills mode')
+        try:
+            # Force any single exposure to be halted
+            self.camera.stop_video_capture()
+            self.camera.stop_exposure()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            pass
+        print('Capturing a single 8-bit mono image')
+        filename = 'image_mono.jpg'
+        self.camera.set_image_type(asi.ASI_IMG_RAW8)
+        self.camera.capture(filename=filename)
+        print('Saved to %s' % filename)
+        save_control_values(filename, self.camera.get_control_values())
+    def start_video_feed(self):
+        # Enable video mode
+        try:
+            # Force any single exposure to be halted
+            camera.stop_exposure()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            pass
+        print('Enabling video mode')
+        self.camera.start_video_capture()
+        # Set the timeout, units are ms
+        timeout = (self.camera.get_control_value(asi.ASI_EXPOSURE)[0] / 1000) * 2 + 500
+        self.camera.default_timeout = timeout
+        # Set up window and display images
+        app = wx.App()
+        frame = Frame(self.camera)
+        frame.Center()
+        frame.Show()
+        app.MainLoop()
 
-num_cameras = asi.get_num_cameras()
-if num_cameras == 0:
-    print('No cameras found')
-    sys.exit(0)
-
-cameras_found = asi.list_cameras()  # Models names of the connected cameras
-
-if num_cameras == 1:
-    camera_id = 0
-    print('Found one camera: %s' % cameras_found[0])
-else:
-    print('Found %d cameras' % num_cameras)
-    for n in range(num_cameras):
-        print('    %d: %s' % (n, cameras_found[n]))
-    # TO DO: allow user to select a camera
-    camera_id = 0
-    print('Using #%d: %s' % (camera_id, cameras_found[camera_id]))
-
-camera = asi.Camera(camera_id)
-camera_info = camera.get_camera_property()
-
-# Get all of the camera controls
-print('')
-print('Camera controls:')
-controls = camera.get_controls()
-for cn in sorted(controls.keys()):
-    print('    %s:' % cn)
-    for k in sorted(controls[cn].keys()):
-        print('        %s: %s' % (k, repr(controls[cn][k])))
-
-
-# Use minimum USB bandwidth permitted
-camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandWidth']['MaxValue'])
-
-# Set some sensible defaults. They will need adjusting depending upon
-# the sensitivity, lens and lighting conditions used.
-camera.disable_dark_subtract()
-
-camera.set_control_value(asi.ASI_GAIN, 0)
-camera.set_control_value(asi.ASI_EXPOSURE, 10000)
-camera.set_control_value(asi.ASI_WB_B, 99)
-camera.set_control_value(asi.ASI_WB_R, 75)
-camera.set_control_value(asi.ASI_GAMMA, 50)
-camera.set_control_value(asi.ASI_BRIGHTNESS, 50)
-camera.set_control_value(asi.ASI_FLIP, 0)
+# Initialize object
+imageSensor = ImageSensor()
 
 # 8-BIT MONO IMAGE CAPTURE
-save_image(camera)
+imageSensor.save_image()
 
 # LIVE VIDEO FEED
-start_video_feed(camera)
+imageSensor.start_video_feed()
 
 ## 16-BIT MONO IMAGE CAPTURE
 # print('Capturing a single 16-bit mono image')
